02bd1241a995aa409836cb9967c4a2be8b00b10d
Matt Erickson <matterickson@hotmail.com>
2015-03-30 10:42:39 -0400

    uploaded project

---
+++ MazeMaker/MazeMaker.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace MazeMaker
+{
+    partial class MazeMaker
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.maze = new System.Windows.Forms.Label();
+            this.redoButton = new System.Windows.Forms.Button();
+            this.mazeBox = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.mazeBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // maze
+            // 
+            this.maze.AutoSize = true;
+            this.maze.Font = new System.Drawing.Font("Courier New", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.maze.Location = new System.Drawing.Point(17, 52);
+            this.maze.Name = "maze";
+            this.maze.Size = new System.Drawing.Size(0, 8);
+            this.maze.TabIndex = 0;
+            // 
+            // redoButton
+            // 
+            this.redoButton.Location = new System.Drawing.Point(170, 12);
+            this.redoButton.Name = "redoButton";
+            this.redoButton.Size = new System.Drawing.Size(51, 23);
+            this.redoButton.TabIndex = 1;
+            this.redoButton.Text = "here!";
+            this.redoButton.UseVisualStyleBackColor = true;
+            this.redoButton.Click += new System.EventHandler(this.redoButton_Click);
+            // 
+            // mazeBox
+            // 
+            this.mazeBox.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.mazeBox.Location = new System.Drawing.Point(240, 12);
+            this.mazeBox.Name = "mazeBox";
+            this.mazeBox.Size = new System.Drawing.Size(400, 400);
+            this.mazeBox.TabIndex = 2;
+            this.mazeBox.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(156, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "To re-run Prim\'s Algorithim click:";
+            // 
+            // MazeMaker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(686, 422);
+            this.Controls.Add(this.redoButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.mazeBox);
+            this.Controls.Add(this.maze);
+            this.Name = "MazeMaker";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MazeMaker_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.mazeBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label maze;
+        private System.Windows.Forms.Button redoButton;
+        private System.Windows.Forms.PictureBox mazeBox;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ MazeMaker/MazeMaker.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MazeMaker
+{
+    public partial class MazeMaker : Form
+    {
+        int w=20, h=20;
+        public MazeMaker()
+        {
+            InitializeComponent();
+            makeMaze(w,h);
+        }
+
+        public void makeMaze(int width, int height)
+        {
+            // dimensions of generated maze
+    	    int r=width, c=height;
+
+            char[,] maz = new char[r,c];
+            for(int x = 0; x < r; x++) {
+                for(int y = 0; y < c; y++){
+                    maz[x,y] = '*';
+                }
+            }
+
+            Random rand = new Random(DateTime.UtcNow.Millisecond);
+
+            // select random point and open as start node
+            Point st = new Point((int)(rand.Next(r)), (int)(rand.Next(c)), null);
+            maz[st.r, st.c] = 'S';
+ 
+            // iterate through direct neighbours of node
+            List<Point> frontier = new List<Point>();
+            for(int x=-1;x<=1;x++){
+        	    for(int y=-1;y<=1;y++){
+        		    if(x==0&&y==0||x!=0&&y!=0) //skip the starting point
+        			    continue;
+        		    try{
+        			    if(maz[st.r+x, st.c+y]=='.') 
+							continue;
+        		    }catch(Exception e){ // ignore ArrayIndexOutOfBounds
+        			    continue;
+        		    }
+					
+        		    // add eligible points to frontier
+        		    frontier.Add(new Point(st.r+x,st.c+y,st));
+        	    }
+			}
+			
+            Point last=null;
+            while(frontier.Count>0){
+        	    // pick current node at random
+        	    Point cu = frontier[rand.Next(frontier.Count)];
+                frontier.Remove(cu);
+        	    Point op = cu.opposite();
+        	    try{
+        		    // if both node and its opposite are walls
+        		    if(maz[cu.r, cu.c]=='*'){
+        			    if(maz[op.r, op.c]=='*'){
+ 
+        				    // open path between the nodes
+        				    maz[cu.r, cu.c]='.';
+        				    maz[op.r, op.c]='.';
+ 
+        				    // store last node in order to mark it later
+        				    last = op;
+ 
+        				    // iterate through direct neighbours of node, same as earlier
+        				    for(int x=-1;x<=1;x++)
+				        	    for(int y=-1;y<=1;y++){
+				        		    if(x==0&&y==0||x!=0&&y!=0)
+				        			    continue;
+				        		    try{
+				        			    if(maz[op.r+x, op.c+y]=='.') continue;
+				        		    }catch(Exception e){
+				        			    continue;
+				        		    }
+				        		    frontier.Add(new Point(op.r+x,op.c+y,op));
+				        	    }
+        			    }
+        		    }
+        	    }catch(Exception e){ // ignore NullPointer and ArrayIndexOutOfBounds
+        	    }
+ 
+        	    // if algorithm has resolved, mark end node
+        	    if(frontier.Count == 0)
+        		    maz[last.r, last.c]='E';
+            }
+
+            printToLabel(maz, r, c);
+            printToPicturebox(maz, r, c);
+        }
+
+        private void printToLabel(char[,] maz, int r, int c)
+        {
+            maze.Text = "";
+            // print final maze as text
+            for (int i = 0; i < r; i++)
+            {
+                for (int j = 0; j < c; j++)
+                {
+                    maze.Text += maz[i, j] + " ";
+                }
+                maze.Text += string.Format("{0}", Environment.NewLine);
+            }
+        }
+
+
+        private void printToPicturebox(char[,] maz, int r, int c)
+        {
+            Bitmap drawArea = new Bitmap(mazeBox.Size.Width, mazeBox.Size.Height);
+            mazeBox.Image = drawArea;
+            // Create solid brush.
+            SolidBrush blueBrush = new SolidBrush(Color.Blue);
+            Graphics g = Graphics.FromImage(drawArea);
+            //g.Clear(Color.Black);
+
+            float width = 20.0F;
+            float height = 20.0F;
+            // print final maze
+            for (int i = 0; i < r; i++){
+                for (int j = 0; j < c; j++){
+                    if (maz[i, j] == '*'){
+                        g.FillRectangle(blueBrush, j*width, i*height, width, height);
+                    } else if (maz[i, j] == 'S'){
+                        Font drawFont = new Font("Arial", 16);
+                        SolidBrush drawBrush = new SolidBrush(Color.White);
+                        StringFormat drawFormat = new StringFormat();
+                        // Draw string to screen.
+                        g.DrawString("S", drawFont, drawBrush, j*width, i*height, drawFormat);
+                    } else if (maz[i, j] == 'E'){
+                        Font drawFont = new Font("Arial", 16);
+                        SolidBrush drawBrush = new SolidBrush(Color.White);
+                        StringFormat drawFormat = new StringFormat();
+                        // Draw string to screen.
+                        g.DrawString("E", drawFont, drawBrush, j * width, i * height, drawFormat);
+                    }
+                }
+            }
+        }
+
+        private void redoButton_Click(object sender, EventArgs e)
+        {
+            makeMaze(w,h);
+        }
+
+    }
+
+    class Point{
+    	public int r, c;
+    	public Point parent;
+		
+    	public Point(int x, int y, Point p){
+    		r=x;
+			c=y;
+			parent=p;
+    	}
+    	// compute opposite node given that it is in the other direction from the parent
+    	public Point opposite(){
+    		if(this.r != parent.r)
+    			return new Point(this.r+(this.r<parent.r?-1:0)+(this.r>parent.r?1:0),this.c,this);
+    		if(this.c != parent.c)
+    			return new Point(this.r,this.c+(this.c<parent.c?-1:0)+(this.c>parent.c?1:0),this);
+    		return null;
+    	}
+    }
+}
